446efc736b67b75985e5adced0653d8651341483
AntØn UstinØff <a.a.ustinoff@gmail.com>
2025-10-30 16:22:23 +0400

    chore: update foundation example

--- flutter_badge_manager_foundation/example/lib/main.dart
+++ flutter_badge_manager_foundation/example/lib/main.dart
@@ -1,16 +1,27 @@
-import 'dart:developer';
+import 'dart:async';
+import 'dart:developer' as dev;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_badge_manager_foundation/flutter_badge_manager_foundation.dart';
 
-void main() => runApp(const MyApp());
+void main() => runZonedGuarded<void>(
+  () => runApp(const App()),
+  (e, s) => dev.log('Top level exception: $e\n$s'),
+);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+/// {@template app}
+/// App widget.
+/// {@endtemplate}
+class App extends StatelessWidget {
+  /// {@macro app}
+  const App({super.key});
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(home: _HomeScreen());
+  Widget build(BuildContext context) => const MaterialApp(
+    title: 'Plugin Foundation Example',
+    home: _HomeScreen(),
+  );
 }
 
 /// {@template main}
@@ -38,26 +49,23 @@ class __HomeScreenState extends State<_HomeScreen> {
   }
 
   Future<void> _initPlatformState() async {
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     String supportedString;
     try {
-      bool isSupported =
-          await FlutterBadgeManagerFoundation.instance.isSupported();
-      log('isSupported: $isSupported');
+      bool isSupported = await FlutterBadgeManagerFoundation.instance
+          .isSupported();
+      dev.log('isSupported: $isSupported');
       if (isSupported) {
         supportedString = 'Supported';
       } else {
         supportedString = 'Not supported';
       }
     } on PlatformException {
-      log('error: PlatformException');
+      dev.log('error: PlatformException');
       supportedString = 'Failed to get badge support.';
-    } on Object catch (_, __) {
-      log('error: Object');
+    } on Object catch (_, _) {
+      dev.log('error: Object');
       supportedString = 'Failed to get badge support.';
     }
 
@@ -70,8 +78,9 @@ class __HomeScreenState extends State<_HomeScreen> {
     _count++;
     messenger?.clearSnackBars();
     FlutterBadgeManagerFoundation.instance.update(_count);
-    messenger
-        ?.showSnackBar(SnackBar(content: Text('Badge count updated: $_count')));
+    messenger?.showSnackBar(
+      SnackBar(content: Text('Badge count updated: $_count')),
+    );
   }
 
   void _remove() {
@@ -80,30 +89,31 @@ class __HomeScreenState extends State<_HomeScreen> {
     _count = 0;
     messenger?.clearSnackBars();
     FlutterBadgeManagerFoundation.instance.remove();
-    messenger
-        ?.showSnackBar(SnackBar(content: Text('Badge count updated: $_count')));
+    messenger?.showSnackBar(
+      SnackBar(content: Text('Badge count updated: $_count')),
+    );
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: const Text('Plugin example app')),
-        body: SizedBox.expand(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text('Badge supported: $_supportedString\n'),
-              ElevatedButton(
-                child: const Text('Add badge'),
-                onPressed: () => _addBadge(),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                child: const Text('Remove badge'),
-                onPressed: () => _remove(),
-              ),
-            ],
+    appBar: AppBar(title: const Text('Plugin example app')),
+    body: SizedBox.expand(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 16,
+        children: <Widget>[
+          Text('Badge supported: $_supportedString\n'),
+          ElevatedButton(
+            child: const Text('Add badge'),
+            onPressed: () => _addBadge(),
           ),
-        ),
-      );
+          ElevatedButton(
+            child: const Text('Remove badge'),
+            onPressed: () => _remove(),
+          ),
+        ],
+      ),
+    ),
+  );
 }
